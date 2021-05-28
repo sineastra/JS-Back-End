23a737140ec0e3498029cea1c84dda06b3d7f586
Sineastra <lord.of.light.0002@gmail.com>
2021-05-28 11:13:47 +0300

    added the multifunctional login page

--- 03_expressJsAndTemplating/selfDemo/src/index.js
+++ 03_expressJsAndTemplating/selfDemo/src/index.js
@@ -7,17 +7,31 @@ const port = 3000
 app.engine(
     ".hbs",
     hbs({
+        partialsDir: "partials",
         extname: ".hbs",
     })
 )
 
 app.set("view engine", ".hbs")
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 
+app.get("/", (req, res) => {
+    res.render("index")
+})
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get("/login", (req, res) => {
+    res.render("login", {
+        style: 'loginStyle.css'
+    })
+})
+
+app.get('/congratz', (req, res) => {
+    res.render('congratz')
+})
+
+app.post('/login', (req, res) => {
+    res.redirect('/congratz')
 })
 
 app.listen(3000, () => console.log("AI IM STARTID"))
