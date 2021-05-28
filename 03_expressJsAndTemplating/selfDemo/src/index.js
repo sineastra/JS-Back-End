954414b6194fcb13d0843f27f0bdf5cc81232da6
Sineastra <lord.of.light.0002@gmail.com>
2021-05-28 12:20:43 +0300

    added login page form validator

--- 03_expressJsAndTemplating/selfDemo/src/index.js
+++ 03_expressJsAndTemplating/selfDemo/src/index.js
@@ -4,6 +4,14 @@ const hbs = require("express-handlebars")
 const app = express()
 const port = 3000
 
+const formValidator = (req, res, next) => {
+    if (/.+@.+\..+/g.test(req.body.email)) {
+        next()
+    } else {
+        res.redirect("/bad-login")
+    }
+}
+
 app.engine(
     ".hbs",
     hbs({
@@ -22,16 +30,33 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render("login", {
-        style: 'loginStyle.css'
+        style: "loginStyle.css",
     })
 })
 
-app.get('/congratz', (req, res) => {
-    res.render('congratz')
+app.post("/login", express.urlencoded({ extended: false }), formValidator, (req, res) => {
+    res.redirect("/congratz")
 })
 
-app.post('/login', (req, res) => {
-    res.redirect('/congratz')
+app.get('/register', (req, res) => {
+    res.render('register', {
+        style: "registerStyle.css"
+    })
+})
+
+app.get("/congratz", (req, res) => {
+    res.render("congratz")
+})
+
+
+app.get("/bad-login", (req, res) => res.render("bad-login"))
+
+app.all("*", (req, res) => {
+    console.log(req.status, req.statusCode)
+    res.render("error", {
+        status: "NOT FAUND",
+        statusCode: "4x100+4",
+    })
 })
 
 app.listen(3000, () => console.log("AI IM STARTID"))
