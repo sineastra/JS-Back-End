bd7ce4eab47f64f2d0b6d063ebe39aa186d51bef
Sineastra <lord.of.light.0002@gmail.com>
2021-06-18 21:39:02 +0300

    added routes protections, edit delete on cube

--- 04_Cubicle_Workshop/controllers/details.js
+++ 04_Cubicle_Workshop/controllers/details.js
@@ -2,10 +2,12 @@ module.exports = async (req, res) => {
 	const id = req.params.id
 	const cube = await req.dbController.getCubeById(id)
 	const populated = await cube.populate('accessories').execPopulate()
+	const isOwner = req.user && cube && req.user._id === cube.creatorId
 
 	res.render('details', {
 		title: cube.name,
 		cube,
 		accessories: populated.accessories,
+		isOwner
 	})
 }
