4886c81b0cbd1bcf1818bfaf1c4b433b51891ae3
Sineastra <lord.of.light.0002@gmail.com>
2021-06-20 16:14:15 +0300

    added details page

--- Exam Prep 1/controllers/hotel.js
+++ Exam Prep 1/controllers/hotel.js
@@ -1,4 +1,17 @@
 const router = require('express').Router()
 const { usersOnly } = require('../middlewares/routeGuards.js')
 
-router.get('/details/:id', usersOnly)
+router.get('/details/:id', /*usersOnly*/ async (req, res) => {
+	const hotel = await req.dbServices.hotel.getOneById(req.params.id)
+	req.user = { _id: '60cf26e6f11b685ed8041e62' }
+
+	hotel.isOwn = hotel.owner === req.user._id
+	hotel.alreadyBooked = hotel.usersWhoBookedRoom.includes(req.user._id)
+
+	res.render('details', {
+		title: `${hotel.name} details`,
+		hotel
+	})
+})
+
+module.exports = router
--- Exam Prep 1/db/services/hotel.js
+++ Exam Prep 1/db/services/hotel.js
@@ -4,5 +4,6 @@ module.exports = {
 	getAll: async () => await Hotel.find({}).lean(),
 	writeOne: async (record) => {
 		await new Hotel(record).save()
-	}
+	},
+	getOneById: async (_id) => Hotel.findOne({ _id: _id }).lean()
 }
