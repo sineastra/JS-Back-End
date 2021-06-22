6ce0b462570ee7f2378b22f483abcf9ca214aa9c
Sineastra <lord.of.light.0002@gmail.com>
2021-06-22 22:20:50 +0300

    commiting forgot commits

--- Exam Prep 2/middlewares/dbServices.js
+++ Exam Prep 2/middlewares/dbServices.js
@@ -1,7 +1,7 @@
 const user = require('../db/services/user')
 const play = require('../db/services/play')
 
-// include all the services, which are in db/services, which use db/models.
+// include all the services, which are in db/services, which use
 module.exports = (req, res, next) => {
 	req.dbServices = {
 		user,
