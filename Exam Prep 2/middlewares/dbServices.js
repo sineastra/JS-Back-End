ef39598ae2695557c12b2329e3188041f6dea19a
Sineastra <lord.of.light.0002@gmail.com>
2021-06-22 22:21:23 +0300

    commiting forgot commits

--- Exam Prep 2/middlewares/dbServices.js
+++ Exam Prep 2/middlewares/dbServices.js
@@ -1,7 +1,6 @@
 const user = require('../db/services/user')
 const play = require('../db/services/play')
 
-// include all the services,
 module.exports = (req, res, next) => {
 	req.dbServices = {
 		user,
