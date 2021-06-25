5f625e66904e8940333123e22f8642d893dfa4c2
Sineastra <lord.of.light.0002@gmail.com>
2021-06-25 23:53:57 +0300

    added functionality

--- Exam Prep 3 - Shoe Shelf/middlewares/dbServices.js
+++ Exam Prep 3 - Shoe Shelf/middlewares/dbServices.js
@@ -1,7 +1,7 @@
 const user = require("../db/services/user")
 const custom = require("../db/services/custom")
 
-// include all the services, which are in db/services, which use d
+// include all the services, which are in db/services, which use
 module.exports = (req, res, next) => {
     req.dbServices = {
         user,
