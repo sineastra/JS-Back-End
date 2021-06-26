2d17945bfb35d8ecc4b19b301f2a9bc4d5a97282
Sineastra <lord.of.light.0002@gmail.com>
2021-06-26 17:09:04 +0300

    added functionality

--- Exam Prep 4 - Tutorials/controllers/user.js
+++ Exam Prep 4 - Tutorials/controllers/user.js
@@ -62,7 +62,7 @@ router.post(
 		.withMessage("Passwords do not match!"),
 	register,
 	login,
-) // VALIDATORS, AFTER
+) // VALIDATORS,
 
 // Login route
 router.get("/login", guestsOnly, (req, res) => res.render("login"))
